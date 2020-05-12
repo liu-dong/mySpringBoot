517b5bce4ce1ea5eb4044f9c3f4b14aa0fe6bed7
liudong <964708273@qq.com>
2020-05-12 09:27:40 +0800

    添加菜单列表

--- src/main/java/com/dong/web/model/SystemMenuBean.java
+++ src/main/java/com/dong/web/model/SystemMenuBean.java
@@ -12,12 +12,12 @@ public class SystemMenuBean {
     private String id;
     private String parentId;
     private String menuName;
-    private int menuLevel;//菜单级别（一级菜单、二级菜单）
+    private Integer menuLevel;//菜单级别（一级菜单、二级菜单）
     private String menuIcon;//菜单图标
     private Integer menuOrder;//菜单排序
     private String menuUrl;//菜单地址
     private String menuPath;//菜单路径
     private Integer menuStatus;//菜单状态，是否显示
-    private int hasChild;//是否有子菜单
+    private Integer hasChild;//是否有子菜单
 }
 
